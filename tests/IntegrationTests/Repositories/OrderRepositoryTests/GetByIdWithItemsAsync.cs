7913fdc0338ded21700508f72d44828209f0aef2
Kendra Havens <kendrahavens@users.noreply.github.com>
2021-02-06 00:36:59 +0000

    Add tests

--- tests/IntegrationTests/Repositories/OrderRepositoryTests/GetByIdWithItemsAsync.cs
+++ tests/IntegrationTests/Repositories/OrderRepositoryTests/GetByIdWithItemsAsync.cs
@@ -57,5 +57,39 @@ namespace Microsoft.eShopWeb.IntegrationTests.Repositories.OrderRepositoryTests
             Assert.Equal(itemOneUnits, orderFromRepo.OrderItems.SingleOrDefault(x => x.UnitPrice == itemOneUnitPrice).Units);
             Assert.Equal(itemTwoUnits, orderFromRepo.OrderItems.SingleOrDefault(x => x.UnitPrice == itemTwoUnitPrice).Units);
         }
+        
+                [Fact]
+        public async Task GetOrderAndItemsByOrderIdWhenMultipleOrdersPresent2()
+        {
+            //Arrange
+            var itemOneUnitPrice = 5.50m;
+            var itemOneUnits = 2;
+            var itemTwoUnitPrice = 7.50m;
+            var itemTwoUnits = 5;
+
+            var firstOrder = OrderBuilder.WithDefaultValues();
+            _catalogContext.Orders.Add(firstOrder);
+            int firstOrderId = firstOrder.Id;
+
+            var secondOrderItems = new List<OrderItem>();
+            secondOrderItems.Add(new OrderItem(OrderBuilder.TestCatalogItemOrdered, itemOneUnitPrice, itemOneUnits));
+            secondOrderItems.Add(new OrderItem(OrderBuilder.TestCatalogItemOrdered, itemTwoUnitPrice, itemTwoUnits));
+            var secondOrder = OrderBuilder.WithItems(secondOrderItems);
+            _catalogContext.Orders.Add(secondOrder);
+            int secondOrderId = secondOrder.Id;
+
+            _catalogContext.SaveChanges();
+
+            //Act
+            var orderFromRepo = await _orderRepository.GetByIdWithItemsAsync(secondOrderId);
+
+            //Assert
+            Assert.Equal(secondOrderId, orderFromRepo.Id);
+            Assert.Equal(secondOrder.OrderItems.Count, orderFromRepo.OrderItems.Count);
+            Assert.Equal(1, orderFromRepo.OrderItems.Count(x => x.UnitPrice == itemOneUnitPrice));
+            Assert.Equal(1, orderFromRepo.OrderItems.Count(x => x.UnitPrice == itemTwoUnitPrice));
+            Assert.Equal(itemOneUnits, orderFromRepo.OrderItems.SingleOrDefault(x => x.UnitPrice == itemOneUnitPrice).Units);
+            Assert.Equal(itemTwoUnits, orderFromRepo.OrderItems.SingleOrDefault(x => x.UnitPrice == itemTwoUnitPrice).Units);
+        }
     }
 }
